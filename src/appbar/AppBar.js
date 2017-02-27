ad9a8d2714d3a2b8bce74f153ca825ea6d670568
ultmaster <scottyugochang@hotmail.com>
2017-02-27 21:38:59 +0800

    update: sign in form

--- src/appbar/AppBar.js
+++ src/appbar/AppBar.js
@@ -1,63 +1,75 @@
 import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react'
+import Sign from '../user/Sign';
 
 export default class AppBar extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { activeItem: 'home' };
+    this.state = {
+      activeItem: 'home',
+      signItemOpen: false,
+      signItemSelected: 0
+    };
   }
 
   handleItemClick = (e, { name }) => {
-    this.setState({ activeItem: name });
+    if (name === 'sign') {
+      this.setState({ signItemOpen: true });
+    } else {
+      this.setState({activeItem: name});
+    }
   };
 
   render() {
     const { activeItem } = this.state;
 
     return (
-      <Menu className="large top fixed">
-        <Menu.Item
-          name="home"
-          active={false}
-          onClick={this.handleItemClick}
-        >
-          <img src='http://semantic-ui.com/images/logo.png' />
-        </Menu.Item>
-
-        <Menu.Item
-          name='problems'
-          active={activeItem === 'problems'}
-          onClick={this.handleItemClick}
-        >
-          Problems
-        </Menu.Item>
-
-        <Menu.Item
-          name='competitions'
-          active={activeItem === 'competitions'}
-          onClick={this.handleItemClick}
-        >
-          Competitions
-        </Menu.Item>
-
-        <Menu.Item
-          name='board'
-          active={activeItem === 'board'}
-          onClick={this.handleItemClick}
-        >
-          Board
-        </Menu.Item>
-        <Menu.Menu position="right">
+      <div>
+        <Menu className="large top fixed">
           <Menu.Item
-            name="sign"
+            name="home"
             active={false}
             onClick={this.handleItemClick}
           >
-            Sign In / Sign Up
+            <img src='http://semantic-ui.com/images/logo.png' />
           </Menu.Item>
-        </Menu.Menu>
-      </Menu>
+
+          <Menu.Item
+            name='problems'
+            active={activeItem === 'problems'}
+            onClick={this.handleItemClick}
+          >
+            Problems
+          </Menu.Item>
+
+          <Menu.Item
+            name='competitions'
+            active={activeItem === 'competitions'}
+            onClick={this.handleItemClick}
+          >
+            Competitions
+          </Menu.Item>
+
+          <Menu.Item
+            name='board'
+            active={activeItem === 'board'}
+            onClick={this.handleItemClick}
+          >
+            Board
+          </Menu.Item>
+          <Menu.Menu position="right">
+            <Menu.Item
+              name="sign"
+              active={false}
+              onClick={this.handleItemClick}
+            >
+              Sign In / Sign Up
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+        <Sign open={this.state.signItemOpen} onClose={() => this.setState({ signItemOpen: false})} />
+      </div>
     )
   }
 }
--- src/user/Sign.js
+++ src/user/Sign.js
@@ -1,29 +1,12 @@
 import React from 'react';
-import TextField from 'material-ui/TextField';
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
-import Snackbar from 'material-ui/Snackbar';
-import muiTheme from '../main/theme';
-import Settings from '../settings';
-
-const $ = require('jquery');
-
-const customContentStyle = {
-  maxWidth: 400
-};
-
-const inputStyles = {
-  textField: {
-    display: 'block'
-  }
-};
+import { Button, Header, Image, Modal, Input, Segment, Grid } from 'semantic-ui-react';
 
 class Sign extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {open: false, successOpen: false, selectedIndex: 0};
-    this.handleClose = props.onClose;
+    this.close = props.onClose;
     this.select = props.onSelected;
   }
 
@@ -49,85 +32,92 @@ class Sign extends React.Component {
     }
     console.log(JSON.stringify(data));
     // SEND REQUEST
-    $.ajax({
-      type: 'POST',
-      url: source,
-      data: data,
-      error: function(data) {
-        for (const key in data.responseJSON) {
-          if (data.responseJSON.hasOwnProperty(key) && this.refs.hasOwnProperty(key)) {
-            this.refs[key].setState({errorText: data.responseJSON[key]});
-          }
-        }
-      }.bind(this),
-      success: function(data) {
-        console.log(data.responseJSON);
-        this.handleClose();
-        this.setState({successOpen: true});
-      }.bind(this)
-    });
+    // $.ajax({
+    //   type: 'POST',
+    //   url: source,
+    //   data: data,
+    //   error: function(data) {
+    //     for (const key in data.responseJSON) {
+    //       if (data.responseJSON.hasOwnProperty(key) && this.refs.hasOwnProperty(key)) {
+    //         this.refs[key].setState({errorText: data.responseJSON[key]});
+    //       }
+    //     }
+    //   }.bind(this),
+    //   success: function(data) {
+    //     console.log(data.responseJSON);
+    //     this.handleClose();
+    //     this.setState({successOpen: true});
+    //   }.bind(this)
+    // });
 
   };
 
   render() {
-    const actions = [
-      <FlatButton
-        label="Cancel"
-        onTouchTap={this.handleClose}
-      />,
-      <FlatButton
-        label="Submit"
-        secondary={true}
-        onTouchTap={this.signSubmit}
-      />,
-    ];
-
-    let title = 'Sign In Now';
-    let content = [
-      <form>
-        <TextField errorText="" ref="username" style={inputStyles.textField} hintText="Username"/>
-        <br/>
-        <TextField errorText="" ref="password" style={inputStyles.textField} hintText="Password" type="password"/>
-        <br/>
-      </form>,
-      <a onClick={() => this.select(1)}
-         style={{cursor: 'pointer', color: muiTheme.palette.primary1Color}}>
-        Do not have an account?</a>
-    ];
-    if (this.state.selectedIndex == 1) {
-      title = 'Create an account';
-      content = [
-        <form>
-          <TextField errorText="" ref="email" style={inputStyles.textField} hintText="Email" type="email" key="email"/>
-          <br/>
-          <TextField errorText="" ref="username" style={inputStyles.textField} hintText="Username" key="username"/>
-          <br/>
-          <TextField errorText="" ref="password" style={inputStyles.textField} hintText="Password" type="password" key="password"/>
-          <br/>
-        </form>,
-        <a onClick={() => this.select(0)} style={{cursor: 'pointer'}}>Click here if you already have an account.</a>
-      ]
-    }
+    // const actions = [
+    //   <FlatButton
+    //     label="Cancel"
+    //     onTouchTap={this.handleClose}
+    //   />,
+    //   <FlatButton
+    //     label="Submit"
+    //     secondary={true}
+    //     onTouchTap={this.signSubmit}
+    //   />,
+    // ];
+    //
+    // let title = 'Sign In Now';
+    // let content = [
+    //   <form>
+    //     <TextField errorText="" ref="username" style={inputStyles.textField} hintText="Username"/>
+    //     <br/>
+    //     <TextField errorText="" ref="password" style={inputStyles.textField} hintText="Password" type="password"/>
+    //     <br/>
+    //   </form>,
+    //   <a onClick={() => this.select(1)}
+    //      style={{cursor: 'pointer', color: muiTheme.palette.primary1Color}}>
+    //     Do not have an account?</a>
+    // ];
+    // if (this.state.selectedIndex == 1) {
+    //   title = 'Create an account';
+    //   content = [
+    //     <form>
+    //       <TextField errorText="" ref="email" style={inputStyles.textField} hintText="Email" type="email" key="email"/>
+    //       <br/>
+    //       <TextField errorText="" ref="username" style={inputStyles.textField} hintText="Username" key="username"/>
+    //       <br/>
+    //       <TextField errorText="" ref="password" style={inputStyles.textField} hintText="Password" type="password" key="password"/>
+    //       <br/>
+    //     </form>,
+    //     <a onClick={() => this.select(0)} style={{cursor: 'pointer'}}>Click here if you already have an account.</a>
+    //   ]
+    // }
 
     return (
-      <div>
-        <Dialog
-          title={title}
-          actions={actions}
-          modal={false}
-          open={this.state.open}
-          contentStyle={customContentStyle}
-          onRequestClose={this.handleClose}
-        >
-          {content}
-        </Dialog>
-        <Snackbar
-          open={this.state.successOpen}
-          message="Success!"
-          autoHideDuration={4000}
-          onRequestClose={this.handleRequestClose}
-        />
-      </div>
+      <Modal open={this.state.open} closeOnDimmerClick={true} onClose={this.close} size="small">
+        <Modal.Header>Sign In</Modal.Header>
+        <Modal.Content image>
+          <Modal.Description>
+            <div className="ui stackable divided equal height stackable grid">
+              <div className="eight wide column">
+                <Input size="large" placeholder='Username' />
+                <br /><br />
+                <Input size="large" placeholder='Password' />
+              </div>
+              <div className="eight wide column">
+                <p>Do you not have an account?</p>
+                <Button basic primary>Sign Up Now</Button>
+              </div>
+            </div>
+
+          </Modal.Description>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color='black' onClick={this.close}>
+            Nope
+          </Button>
+          <Button positive icon='checkmark' labelPosition='right' content="Yep, that's me" onClick={this.close} />
+        </Modal.Actions>
+      </Modal>
     );
   }
 }
